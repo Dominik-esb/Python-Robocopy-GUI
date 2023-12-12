0f561c2e32ba6d3ffeb7be80e7c717b638fdf02e
dominik_esb <d.eisenberg@outlook.de>
2023-12-13 00:08:21 +0100

    Refactor grid layout and button placement in SettingsView

--- robo-gui/view/setting_view.py
+++ robo-gui/view/setting_view.py
@@ -7,22 +7,27 @@ class SettingsView(customtkinter.CTkFrame):
         self.parent = parent
         self.parent.protocol("WM_DELETE_WINDOW", self.on_closing)
 
+        # ============ configure grid layout (4x4) ============
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
         self.btn_save = customtkinter.CTkButton(
             master=self, text="Save", command=lambda: self.save(window=parent))
-        self.btn_save.grid(row=1,
-                           column=0, columnspan=2, pady=20, padx=20)
+        self.btn_save.grid(row=3,
+                           column=2, pady=20, padx=20)
 
         self.appearance_mode_label = customtkinter.CTkLabel(
             master=self, text="Appearance Mode:", anchor="w"
         )
-        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_label.grid(row=1, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(
             master=self,
             values=["Light", "Dark", "System"],
             command=self.change_appearance_mode_event,
         )
         self.appearance_mode_optionemenu.grid(
-            row=6, column=0, padx=20, pady=(10, 10))
+            row=2, column=0, padx=20, pady=(10, 10))
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
