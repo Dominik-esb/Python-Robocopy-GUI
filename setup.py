6062f5933239e653eea9e2dc3cef12d278f94735
dominik_esb <d.eisenberg@outlook.de>
2023-12-12 19:25:56 +0100

    Add setup.py file with package information

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='your_package_name',
+    version='0.1',
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='A short description of your package',
+    packages=['your_package'],
+    install_requires=[
+        # List any dependencies your package requires
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
