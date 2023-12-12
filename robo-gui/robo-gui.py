2354004c48ef28f6f43f0b925811eb57adda06f0
dominik_esb <d.eisenberg@outlook.de>
2023-12-12 17:09:02 +0100

    Refactor code to improve performance and readability

--- robo-gui/robo-gui.py
+++ robo-gui/robo-gui.py
@@ -25,7 +25,7 @@ class App(customtkinter.CTk):
             self.build_ui()
 
     def build_ui(self):
-        # configure window
+        # ============ configure window ============
         self.title("Robocopy GUI")
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
         self.update()
@@ -33,32 +33,37 @@ class App(customtkinter.CTk):
 
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        # configure grid layout (4x4)
+        # ============ configure grid layout (4x4) ============
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # create sidebar frame with widgets
+        # ============ create sidebar frame with widgets ============
         self.sidebar_frame = customtkinter.CTkFrame(
             self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
+
         self.logo_label = customtkinter.CTkLabel(
             self.sidebar_frame,
             text="Robo-GUI",
             font=customtkinter.CTkFont(size=20, weight="bold"),
         )
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_1 = customtkinter.CTkButton(
-            self.sidebar_frame, command=self.sidebar_button_event
-        )
-        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(
-            self.sidebar_frame, command=self.sidebar_button_event
-        )
-        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
 
-        # Settings Button (in the position of the Theme Switch)
+        self.sidebar_button_start = customtkinter.CTkButton(
+            self.sidebar_frame, command=self.sidebar_button_event,
+            fg_color="#017550", hover_color="#014f42",
+        )
+        self.sidebar_button_start.grid(row=1, column=0, padx=20, pady=10)
+
+        self.sidebar_button_stop = customtkinter.CTkButton(
+            self.sidebar_frame, command=self.sidebar_button_event,
+            fg_color="#7e0030", hover_color="#60002d",
+        )
+        self.sidebar_button_stop.grid(row=2, column=0, padx=20, pady=10)
+
+        # ============ Settings Button ============
         self.sidebar_button_settings = customtkinter.CTkButton(
             master=self.sidebar_frame,
             # fg_color="#2a2d2e",
@@ -70,19 +75,8 @@ class App(customtkinter.CTk):
         self.sidebar_button_settings.grid(
             row=8, column=0, padx=20, pady=(10, 20))
 
-        self.appearance_mode_label = customtkinter.CTkLabel(
-            self.sidebar_frame, text="Appearance Mode:", anchor="w"
-        )
-        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(
-            self.sidebar_frame,
-            values=["Light", "Dark", "System"],
-            command=self.change_appearance_mode_event,
-        )
-        self.appearance_mode_optionemenu.grid(
-            row=6, column=0, padx=20, pady=(10, 10))
         self.scaling_label = customtkinter.CTkLabel(
-            self.sidebar_frame, text="UI Scaling:", anchor="w"
+            self.sidebar_frame, text="Settings menue:", anchor="w"
         )
         self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
 
@@ -94,96 +88,32 @@ class App(customtkinter.CTk):
         # self.scaling_optionemenu.grid(row=8, column=0, padx=20,
         # pady=(10, 20))
 
-        # create textbox
+        # ============ create textbox ============
         self.textbox = customtkinter.CTkTextbox(self, width=250)
-        self.textbox.grid(row=0, column=1, padx=(
+        self.textbox.grid(row=0, column=1, rowspan=2, padx=(
             20, 0), pady=(20, 0), sticky="nsew")
 
-        # create tabview
-        self.tabview = customtkinter.CTkTabview(self, width=250)
-        self.tabview.grid(row=0, column=2, padx=(
-            20, 0), pady=(20, 0), sticky="nsew")
-        self.tabview.add("CTkTabview")
-        self.tabview.add("Tab 2")
-        self.tabview.add("Tab 3")
-        self.tabview.tab("CTkTabview").grid_columnconfigure(
-            0, weight=1
-        )  # configure grid of individual tabs
-        self.tabview.tab("Tab 2").grid_columnconfigure(0, weight=1)
+        # ============ create tabview ============
+        self.scrollable_frame_test = customtkinter.CTkFrame(
+            self, width=140, corner_radius=0)
+        self.scrollable_frame_test.grid(
+            row=2, column=1, padx=(20, 0), pady=(20, 20), sticky="nsew"
+        )
+        self.scrollable_frame_test.grid_columnconfigure(0, weight=1)
+        self.scrollable_frame_test_switches = []
+        for i in range(3):
+            switch = customtkinter.CTkSwitch(
+                master=self.scrollable_frame_test, text=f"CTkSwitch {i}"
+            )
+            switch.grid(row=i, column=0, padx=10, pady=(0, 20))
+            self.scrollable_frame_test_switches.append(switch)
 
-        self.optionmenu_1 = customtkinter.CTkOptionMenu(
-            self.tabview.tab("CTkTabview"),
-            dynamic_resizing=False,
-            values=["Value 1", "Value 2", "Value Long Long Long"],
-        )
-        self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.combobox_1 = customtkinter.CTkComboBox(
-            self.tabview.tab("CTkTabview"),
-            values=["Value 1", "Value 2", "Value Long....."],
-        )
-        self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
-        self.string_input_button = customtkinter.CTkButton(
-            self.tabview.tab("CTkTabview"),
-            text="Open CTkInputDialog",
-            command=self.open_input_dialog_event,
-        )
-        self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
-        self.label_tab_2 = customtkinter.CTkLabel(
-            self.tabview.tab("Tab 2"), text="CTkLabel on Tab 2"
-        )
-        self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
-
-        # create slider and progressbar frame
-        self.slider_progressbar_frame = customtkinter.CTkFrame(
-            self, fg_color="transparent"
-        )
-        self.slider_progressbar_frame.grid(
-            row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew"
-        )
-        self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
-        self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
-        self.seg_button_1 = customtkinter.CTkSegmentedButton(
-            self.slider_progressbar_frame
-        )
-        self.seg_button_1.grid(
-            row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
-        )
-        self.progressbar_1 = customtkinter.CTkProgressBar(
-            self.slider_progressbar_frame)
-        self.progressbar_1.grid(
-            row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
-        )
-        self.progressbar_2 = customtkinter.CTkProgressBar(
-            self.slider_progressbar_frame)
-        self.progressbar_2.grid(
-            row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
-        )
-        self.slider_1 = customtkinter.CTkSlider(
-            self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4
-        )
-        self.slider_1.grid(row=3, column=0, padx=(
-            20, 10), pady=(10, 10), sticky="ew")
-        self.slider_2 = customtkinter.CTkSlider(
-            self.slider_progressbar_frame, orientation="vertical"
-        )
-        self.slider_2.grid(
-            row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10),
-            sticky="ns"
-        )
-        self.progressbar_3 = customtkinter.CTkProgressBar(
-            self.slider_progressbar_frame, orientation="vertical"
-        )
-        self.progressbar_3.grid(
-            row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10),
-            sticky="ns"
-        )
-
-        # create scrollable frame
+        # ============ create scrollable frame ============
         self.scrollable_frame = customtkinter.CTkScrollableFrame(
-            self, label_text="CTkScrollableFrame"
+            self, label_text="Backup Jobs"
         )
         self.scrollable_frame.grid(
-            row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
+            row=0, rowspan=3, column=2, padx=(20, 0), pady=(20, 20), sticky="nsew"
         )
         self.scrollable_frame.grid_columnconfigure(0, weight=1)
         self.scrollable_frame_switches = []
@@ -194,33 +124,16 @@ class App(customtkinter.CTk):
             switch.grid(row=i, column=0, padx=10, pady=(0, 20))
             self.scrollable_frame_switches.append(switch)
 
-        # set default values
+        # ============ set default values ============
+        self.sidebar_button_start.configure(text="Start Copy")
+        self.sidebar_button_stop.configure(text="Stop Copy")
         self.scrollable_frame_switches[0].select()
         self.scrollable_frame_switches[4].select()
-        self.appearance_mode_optionemenu.set("Dark")
         # self.scaling_optionemenu.set("100%")
-        self.optionmenu_1.set("CTkOptionmenu")
-        self.combobox_1.set("CTkComboBox")
-        self.slider_1.configure(command=self.progressbar_2.set)
-        self.slider_2.configure(command=self.progressbar_3.set)
-        self.progressbar_1.configure(mode="indeterminnate")
-        self.progressbar_1.start()
         self.textbox.insert(
             "0.0",
             "Log\n\n"
         )
-        self.seg_button_1.configure(
-            values=["CTkSegmentedButton", "Value 2", "Value 3"])
-        self.seg_button_1.set("Value 2")
-
-    def open_input_dialog_event(self):
-        dialog = customtkinter.CTkInputDialog(
-            text="Type in a number:", title="CTkInputDialog"
-        )
-        print("CTkInputDialog:", dialog.get_input())
-
-    def change_appearance_mode_event(self, new_appearance_mode: str):
-        customtkinter.set_appearance_mode(new_appearance_mode)
 
     def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
