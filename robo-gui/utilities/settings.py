6ae012863688e8b117d58d9af1c1eb297d2a7944
Dominik <d.business@outlook.de>
2023-12-13 12:46:39 +0100

    Update json file handling functions

--- robo-gui/utilities/settings.py
+++ robo-gui/utilities/settings.py
@@ -1,12 +1,15 @@
+"""This module contains functions for loading and saving json files"""
 import json
 
 
 def load_json(file_path):
-    with open(file_path, 'r') as file:
+    """loads a json file"""
+    with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
 
 
 def save_json(file_path, data):
-    with open(file_path, 'w') as file:
+    """saves a json file"""
+    with open(file_path, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4)
