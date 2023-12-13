f9661e487fba63358ef4b1c010e4c723fd3c5468
Dominik <d.business@outlook.de>
2023-12-13 12:43:28 +0100

    Add TabView class with two tabs

---
+++ robo-gui/view/tab_view.py
@@ -0,0 +1,17 @@
+"""Tabview with two tabs."""
+import customtkinter
+
+
+class MyTabView(customtkinter.CTkTabview):
+    """Tabview with two tabs."""
+
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        # create tabs
+        self.add("tab 1")
+        self.add("tab 2")
+
+        # add widgets on tabs
+        self.label = customtkinter.CTkLabel(master=self.tab("tab 1"))
+        self.label.grid(row=0, column=0, padx=20, pady=10)
