6bc943ae2f1f328ae728f8a56df0ba75423152f9
Dominik <d.business@outlook.de>
2023-12-18 00:28:34 +0100

    Refactor Smart class initialization and get_smart_data method

--- robo-gui/utilities/smart_handler.py
+++ robo-gui/utilities/smart_handler.py
@@ -9,16 +9,17 @@ class Smart:
 
     def __init__(self):
         """initializes the Smart class"""
-        self.devlist = DeviceList()
-        self.pattern = r'sn:(\S+)'
+        self.devlist = []
+        self.pattern = r"sn:(\S+)"
         self.list = []
 
     def get_smart_data(self):
         """returns the SMART data"""
-        for device in self.devlist:
-            self.list.append(re.search(self.pattern, device))
-            print(self.list)
+        self.devlist = DeviceList()
+        print(type(self.devlist))
 
         return self.devlist
+
+
 s = Smart()
 s.get_smart_data()
